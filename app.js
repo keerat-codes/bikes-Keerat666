54d444e5af4f39b8ee23177faacd4257fa37fa06
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-14 23:58:33 +0530

    Updated App.js for bikeNames route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const bikeNamesRoute = require('./routes/bikeNames');
+app.use('/bikeNames', bikeNamesRoute);
 
 //Please don't delete this health API
 app.use('/api/health', (req, res) => {
